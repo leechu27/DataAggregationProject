348058a394775f89b036da5e49fe8675a4db5d9e
moladan123 <alon.djurinsky@gmail.com>
2018-10-27 21:52:35 -0400

    created a csv parser class with a sample csv file

---
+++ code/src/c01_project/database/CSVParser.java
@@ -0,0 +1,13 @@
+package c01_project.database;
+
+public class CSVParser {
+
+  /**
+   *  Input a csv file with the
+   */
+  public String parseCSV(String filename) {
+
+    return null;
+  }
+
+}
