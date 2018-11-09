3e49f204db4f903ecdba51182134ebfa2bee9d22
Christopher Recillo <topher452@hotmail.com>
2018-11-08 19:27:59 -0500

    added test ideas for DatabaseSelector

---
+++ code/src/test/java/com/c01_project/database/selector/DatabaseSelectorTest.java
@@ -0,0 +1,44 @@
+package c01_project.database.selector;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+public class DatabaseSelectorTest {
+
+  @BeforeAll
+  public void setUp() throws Exception {}
+
+  @Test
+  @DisplayName("select one column")
+  public void testOneColumn() {
+
+  }
+
+  @Test
+  @DisplayName("select two columns")
+  public void testTwoColumns() {
+
+  }
+  
+  @Test
+  @DisplayName("select * from a table")
+  public void testAllColumns() {
+
+  }
+  
+  @Test
+  @DisplayName("select no columns")
+  public void testNoColumns() {
+
+  }
+  
+  @Test
+  @DisplayName("select an incorrect column")
+  public void test() {
+
+  }
+}
