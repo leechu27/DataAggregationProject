8d236a7f96c614ce27482d69bf9a8e9fd56bb16f
Christopher Recillo <topher452@hotmail.com>
2018-11-08 19:52:12 -0500

    Added ReportTest file but mostly empty, relocated UserQuery for compilation reasons

---
+++ code/src/test/java/com/c01_project/reports/ReportTest.java
@@ -0,0 +1,18 @@
+package c01_project.reports;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ReportTest {
+
+  @BeforeAll
+  public void setUp() throws Exception {}
+
+  @Test
+  @DisplayName("")
+  public void test() {
+
+  }
+
+}
