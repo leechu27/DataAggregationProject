7b3479be0402e0c3272adb7553604bc8c9af30b6
Christopher Recillo <topher452@hotmail.com>
2018-11-12 10:16:05 -0500

    Fixed beforeAll database initalization

--- code/src/test/java/com/c01_project/database/selector/DatabaseSelectorTest.java
+++ code/src/test/java/com/c01_project/database/selector/DatabaseSelectorTest.java
@@ -1,28 +1,36 @@
 package c01_project.database.selector;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import c01_project.database.CSVParser;
 import c01_project.database.DatabaseQuery;
-import c01_project.database.PendingDatabaseEntry;
+import c01_project.database.databaseSetup;
 
 
 public class DatabaseSelectorTest {
   
-  private final String TEST_ROOT = "src/test/resources/testICareTemplates/";
+  private final static String TEST_ROOT = "src/test/resources/testICareTemplates/";
+  private final static String DATABASE_PATH = "test.db";
   private static CSVParser parser;
   private static DatabaseQuery query;
 
   @BeforeAll
-  public void setUp() {
-    parser = new CSVParser("test.db");
-    query = new DatabaseQuery("test.db");
-    PendingDatabaseEntry entry = parser.parseCSVBasicICareTemplate(TEST_ROOT + "normal_mulitple_linens.csv");
+  public static void beforeAll() {
+    databaseSetup.createNewDatabase(DATABASE_PATH);
+    databaseSetup.initializeNewTables(DATABASE_PATH);
+    parser = new CSVParser(DATABASE_PATH);
+    com.c01_project.database.PendingDatabaseEntryInterface entry = parser.parseCSVBasicICareTemplate(TEST_ROOT + "normal_multiple_linens.csv");
+    entry.dumpIntoDatabase(DATABASE_PATH);
+    entry.clear();
+    query = new DatabaseQuery(DATABASE_PATH);
   }
 
   @Test
@@ -33,12 +41,22 @@ public class DatabaseSelectorTest {
     List<String> outcome = new ArrayList<String>();
     expected.add("Mohammed Ali");
     expected.add("Terry Suns");
-    ResultSet result = DatabaseSelector.selectColumnms(query, "basic_data", columns);
-    while (result.next()) {
-      outcome.add(result.getString("name"));
-    }
-    result.close();
-    assertEquals(expected, outcome);
+	try {
+	  ResultSet result = DatabaseSelector.selectColumnms(query, "basic_data", columns);
+	  while (result.next()) {
+	    outcome.add(result.getString("name"));
+		}
+	  result.close();
+	  assertEquals(expected, outcome);
+	} catch (SQLException sql) {
+	  fail("Unexpected SQLException has been thrown");
+	} catch (DatabaseNullException dne) {
+	  fail("Database is null");
+	} catch (NullColumnsException nce) {
+	  fail("Columns is null");
+	} catch (NullTableException nte) {
+	  fail("table is null");
+	}
   }
 
   @Test
