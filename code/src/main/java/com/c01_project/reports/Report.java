22f1f8081d017eb04dbc45db36782ecae47c0f91
Christopher Recillo <topher452@hotmail.com>
2018-11-16 23:21:40 -0500

    refactored Report in code folder, removed test suite for refactoring

--- code/src/main/java/com/c01_project/reports/Report.java
+++ code/src/main/java/com/c01_project/reports/Report.java
@@ -1,112 +1,18 @@
 package c01_project.reports;
 
-import c01_project.database.DatabaseQuery;
-import c01_project.gui.FileExistsException;
-import c01_project.gui.InvalidFileException;
-import java.io.IOException;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.io.File;
-import java.io.FileWriter;
 
-public class Report {
-
-	private String location;
-	private String name;
-	private final String fileRegex = ".+[.]csv";
-
-	/* Constructor for report, creates a file to put the report in given that the file does not exist yet
-	 * 
-	 * @param report name that includes .csv at the end
-	 * @param file path from root directory to destination (include "\" at the end)
-	 * @throws InvalidFileException if file name is not of .csv format
-	 * @throws FileExistsException if file name is linked to an existing file
-	 */
-	public Report(String location, String name) throws InvalidFileException, FileExistsException {
-		if (name.matches(fileRegex)) {
-			
-			File file = new File(location + name);
-			if (file.exists() && !file.canWrite()) {
-				throw new FileExistsException();
-			}
-			try {
-				if (file.createNewFile()) {
-					this.location = location;
-					this.name = name;
-				} else {
-					throw new FileExistsException();
-				}
-			} catch (IOException e ) {
-				throw new FileExistsException();
-			}
-			
-		} else {
-			throw new InvalidFileException();
-		}
-	}
-	
-	/* writes the requested data from a table in the database to the report file
-	*
-	* @param database the database file to gather the data from
-	* @param table the table name from the database
-	* @param columns of the table
-	* @throws FileNotFoundException if the report file is missing
-	*/
-	public void writeReport(DatabaseQuery database, String table, String[] columns) throws FileNotFoundException{
-		File file = new File(location + name);
-		if (!(file.exists())) {
-		    throw new FileNotFoundException();
-		}
-		String command;
-		command = "SELECT * FROM " + table + ";";
-		ResultSet data = database.queryWithSQL(command);
-		writeToFile(data, columns);
-		try {
-		    data.close();
-		} catch (SQLException e) {
-            e.printStackTrace();
-        }
-	}
-	
-	/* writes the given String into the report file, returns true if successful
-	*
-	* @throws FileNotFoundException if the original file is missing
-	*/
-	private boolean writeToFile(ResultSet data, String[] columns) throws FileNotFoundException{
-		try {
-			File file = new File(location + name);
-			if (!file.exists()) {
-				throw new FileNotFoundException();
-			}
-			
-			FileWriter writer = new FileWriter(file, false);
-			for (String header : columns) {
-			  writer.write( header + ",");
-			}
-			while (data.next()) {
-			    for (String column : columns) {
-			        String entry = data.getString(column);
-			        writer.write( entry + ",");
-			    }
-                writer.write("\n");;
-			}
-            writer.close();
-		} catch (IOException e) {
-			return false;
-		} catch (SQLException sql) {
-		    sql.printStackTrace();
-		}
-		return true;
-	}
+public interface Report {
 	
 	/*
-	public static void main(String[] args) {
-		try {
-			Report test = new Report("D:\\Users\\tophe\\Team26\\code\\src\\main\\java\\com\\c01_project\\reports\\", "test.csv");
-			test.writeToFile("WILL YOU SURVIVE?");
-			System.out.println("done :)");
-		} catch (Exception e) {
-			System.out.println("OOPSIE WOOPSIE!! Uwu We made a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!");
-		}
-	}*/
+	 * Outputs the current data to the file location
+	 */
+	public void writeToFile();
+	
+	/*
+	 * adds a new data entry to the graph to be displayed on the file, updates a value if the key exists
+	 * @param key      The key to be set
+	 * @param value    The value of the new key
+	 */
+	public void setNewData(String key, double value);
 }
--- code/src/test/java/com/c01_project/reports/ReportTest.java
+++ code/src/test/java/com/c01_project/reports/ReportTest.java
@@ -2,65 +2,15 @@ package c01_project.reports;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
-
-import java.io.File;
-import javax.print.attribute.standard.PrinterLocation;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import c01_project.database.CSVParser;
-import c01_project.database.DatabaseQuery;
-import c01_project.database.databaseSetup;
-import c01_project.gui.FileExistsException;
-import c01_project.gui.InvalidFileException;
 
 public class ReportTest {
 
-  private final static String DATABASE_PATH = "src/test/resources/testDatabases/test.db";
-  private static Report report;
-  private static DatabaseQuery query;
-
   @BeforeAll
-  public static void beforeAll() throws InvalidFileException, FileExistsException {
-    try {
-      report = new Report("src/test/resources/testReports/", "testReport.csv");
-      query = new DatabaseQuery(DATABASE_PATH);
-    } catch (InvalidFileException i) {
-      System.out.println(i.getMessage());
-    } catch (FileExistsException f) {
-      System.out.println(f.getMessage());
-    }
+  public static void beforeAll() {
+    
   }
 
-  @Test
-  @DisplayName("a single column")
-  public void testOneColumn() {
-    String[] columns = {"name"};
-    try {
-      report.writeReport(query, "basic_data", columns);
-      File expected = new File("src/test/resources/testReports/single_column.csv");
-      File result = new File("src/test/resources/testReports/testReport.csv");
-      assertEquals(expected, result);
-    } catch (FileNotFoundException fne) {
-      fail(fne.getMessage());
-    }
-  }
-
- /* @Test
-  @DisplayName("multiple columns")
-  public void testMultipleColumns() {
-
-  }
-  
-  @Test
-  @DisplayName("report a table")
-  public void testFullTable() {
-
-  }
-  
-  @Test
-  @DisplayName("")
-  public void test() {
-
-  }*/
 }
