72fac7202776c08a1ebcf9fe9993eba8a111b469
moladan123 <alon.djurinsky@gmail.com>
2018-11-17 12:41:59 -0500

    allowed csvParser to work on real templates

--- codeEc/src/database/CSVParser.java
+++ codeEc/src/database/CSVParser.java
@@ -6,76 +6,102 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.util.ArrayList;
+import java.util.List;
 
 public class CSVParser {
 
   // Where the primary ID used to identify each user can be found in the csv
-  private static final int PRIMARY_ID_COLUMN = 0;
-  private String databasePath;
+  private int PRIMARY_ID_COLUMN = 0;
+  private static final String PRIMARY_ID_COL_NAME = "client_validation_id";
+  private static final String DEFAULT_TABLE = "client_profile";
   private PendingDatabaseEntryInterface pde;
 
-  public CSVParser(String databasePath) {
-    this.databasePath = databasePath;
+  public CSVParser() {
     this.pde = new database.PendingDatabaseEntry();
   }
 
-  public CSVParser(String databasePath, PendingDatabaseEntryInterface i) {
-    this.databasePath = databasePath;
+  public CSVParser(PendingDatabaseEntryInterface i) {
     this.pde = i;
   }
-
+  
+  
   /**
-   * reads the given csv file for
-   * @param filename
-   * @return the response given by the database
+   * Reads the csv file with the Team26 format ICare template in it.
+   * 
+   * In order for this to work, the column names must match exactly to
+   * the database schema, or else this will return errors. The following is the format of csvFiles
+   * 
+   * First line must be the name of the table, refer to the schema for this.
+   * Second line must be the column names that are to be added. The first column must contain be the user's
+   * unique id, though the rest of the column names only need to contain the ones that were actually changed
+   * 
+   * The following lines contain one user's information in it.
+   * 
+   * @param csvFile the path to the csvFile
+   * @return the databaseEntryInterface that needs to be checked for conflicts
    */
   public PendingDatabaseEntryInterface parseCSVBasicICareTemplate(String csvFile) {
+	  
+	  PendingDatabaseEntryInterface entry = pde;
 
-    PendingDatabaseEntryInterface entry = pde;
+	    BufferedReader br = null;
+	    String line = "";
+	    String csvSplitBy = ",";
 
-    BufferedReader br = null;
-    String line = "";
-    String csvSplitBy = ",";
+	    String tableName;
+	    String[] columnNames;
+	    ArrayList<String[]> lines;
 
-    String[] columnNames;
-    ArrayList<String[]> lines;
+	    try {
 
-    try {
+	      // getting the names of all the columns
+	      br = new BufferedReader(new FileReader(csvFile));
+	      tableName = br.readLine();
+	      columnNames = br.readLine().split(csvSplitBy);
+	      
+	      // get the location of the id column that is to be matched with
+	      for (int i = 0; i < columnNames.length; i++) {
+	    	  if (columnNames[i].equals(PRIMARY_ID_COL_NAME)) {
+	    		  PRIMARY_ID_COLUMN = i;
+	    		  break;
+	    	  }
+	      }
+	    		  
+	      lines = new ArrayList<>();
 
-      // getting the names of all the columns
-      br = new BufferedReader(new FileReader(csvFile));
-      columnNames = br.readLine().split(csvSplitBy);
-      lines = new ArrayList<>();
+	      // reading the csv
+	      while ((line = br.readLine()) != null) {
+	    	  if (!line.equals("") && (line.split(csvSplitBy).length == columnNames.length)) {
+	    		  String[] patient = line.split(csvSplitBy);  
+	    		  lines.add(patient);
+	    	  }
+	        // TODO need to add escape sequences. This is probably easier done with a library another time
+	      }
 
-      // reading the csv
-      while ((line = br.readLine()) != null) {
-        String[] patient = line.split(csvSplitBy);
-        lines.add(patient);
-      }
+	      // add the actual data
+	      for (String[] user: lines) {
+	        String id = user[PRIMARY_ID_COLUMN];
+	        for (int i = 0; i < columnNames.length; i++) {
+	          if (i != PRIMARY_ID_COLUMN)
+	            entry.addData(id, tableName, columnNames[i], user[i]);
+	        }
+	      }
 
-      // add the actual data
-      for (String[] user: lines) {
-        String id = user[PRIMARY_ID_COLUMN];
-        for (int i = 0; i < columnNames.length; i++) {
-          if (i != PRIMARY_ID_COLUMN)
-            entry.addData(id, "basic_data", columnNames[i], user[i]);
-        }
-      }
-
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    } catch (IOException e) {
-      e.printStackTrace();
-    } finally { // make sure to close the file Regardless of whether there was an error
-      if (br != null) {
-        try {
-          br.close();
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-      }
-    }
-    return entry;
+	    } catch (FileNotFoundException e) {
+	      e.printStackTrace();
+	    } catch (IOException e) {
+	      e.printStackTrace();
+	    } finally { // make sure to close the file Regardless of whether there was an error
+	      if (br != null) {
+	        try {
+	          br.close();
+	        } catch (IOException e) {
+	          e.printStackTrace();
+	        }
+	      }
+	    }
+	    return entry;
   }
 }
