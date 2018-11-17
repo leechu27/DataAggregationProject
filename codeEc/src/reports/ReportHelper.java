7732065e2ccfe457e9a3ee3c700064b2b9e06c3b
Christopher Recillo <topher452@hotmail.com>
2018-11-17 18:38:42 -0500

    progress on ReportHelper

--- codeEc/src/reports/ReportHelper.java
+++ codeEc/src/reports/ReportHelper.java
@@ -1,6 +1,8 @@
 package reports;
 
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import database.DatabaseQuery;
 
@@ -23,8 +25,15 @@ public class ReportHelper {
    * 
    * @param entries the entries to be added into the report, formatted in a way that it can be put into SQL as a condition
    */
-  public void addEntries(List<String> entries) {
-    String base = "";
-    
+  public void addEntries(String table, List<String> entries) throws SQLException {
+    String base = "SELECT COUNT(*) AS rowcount FROM " + table + " WHERE ";
+    String temp;
+    int value;
+    for (String entry : entries) {
+      temp = base + entries;
+      ResultSet result = database.queryWithSQL(temp);
+      result.next();
+      value = result.getInt("rowcount");
+    }
   }
 }
