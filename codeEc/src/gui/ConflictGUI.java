f7bc45aadc199da27c89d93166626d24b855472e
leechu27 <32986341+leechu27@users.noreply.github.com>
2018-11-18 13:34:26 -0500

    Add files via upload

--- codeEc/src/gui/ConflictGUI.java
+++ codeEc/src/gui/ConflictGUI.java
@@ -56,31 +56,41 @@ public class ConflictGUI extends JFrame {
 		contentPane.add(lblConflict);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(168, 154, 479, 95);
+		scrollPane.setBounds(144, 189, 479, 86);
 		contentPane.add(scrollPane);
 		
+		// old data table
 		table = new JTable();
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		table.setSurrendersFocusOnKeystroke(true);
+		table.setFocusable(false);
 		table.setModel(new DefaultTableModel(
 			new Object[][] {
-				{null, null, null},
+				// row 1 of old table
+				{"1", "2", "3", null, null, null, null, null, null, null, null, null, null},
 			},
 			new String[] {
-				"New column", "New column", "New column"
+				// row 2 of old table
+				"New column", "New column", "col 3", "New column", "New column", "New column", "New column", "New column", "New column", "New column", "New column", "New column", "last column"
 			}
-		));
+		) );
 		scrollPane.setViewportView(table);
 		
 		JScrollPane scrollPane_1 = new JScrollPane();
-		scrollPane_1.setBounds(168, 294, 479, 95);
+		scrollPane_1.setBounds(144, 321, 479, 86);
 		contentPane.add(scrollPane_1);
-		
+		// new data table
 		table_1 = new JTable();
+		table_1.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		table_1.setFocusable(false);
 		table_1.setModel(new DefaultTableModel(
 			new Object[][] {
-				{null, null, null},
+				// row 1 of new table
+				{"1", "2", "3", null, null, null, null, null, null, null, null, null},
 			},
 			new String[] {
-				"New column", "New column", "New column"
+				// row 2 of new table
+				"col 1", "col 2", "col 3", "New column", "New column", "New column", "New column", "New column", "New column", "New column", "New column", "New column"
 			}
 		));
 		scrollPane_1.setViewportView(table_1);
@@ -88,7 +98,7 @@ public class ConflictGUI extends JFrame {
 		JRadioButton rdbtnOldData = new JRadioButton("Old data");
 		
 		rdbtnOldData.setSelected(true);
-		rdbtnOldData.setBounds(53, 192, 109, 23);
+		rdbtnOldData.setBounds(53, 192, 81, 23);
 		contentPane.add(rdbtnOldData);
 		
 		JRadioButton rdbtnNewData = new JRadioButton("New data");
@@ -107,7 +117,7 @@ public class ConflictGUI extends JFrame {
 				
 			}
 		});
-		rdbtnNewData.setBounds(53, 324, 109, 23);
+		rdbtnNewData.setBounds(53, 324, 81, 23);
 		contentPane.add(rdbtnNewData);
 		
 		JButton btnSelect = new JButton("Select");
