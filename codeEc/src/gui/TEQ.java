23548bf5b6be8ce21c5bfd3594aa3bd9d660aa3b
leechu27 <32986341+leechu27@users.noreply.github.com>
2018-11-24 23:47:14 -0500

    added directory picker

--- codeEc/src/gui/TEQ.java
+++ codeEc/src/gui/TEQ.java
@@ -21,6 +21,11 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.sql.SQLException;
 
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.util.*;
+
 public class TEQ extends JFrame {
 
 	private JPanel contentPane;
@@ -99,6 +104,21 @@ public class TEQ extends JFrame {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				System.out.println("Create button clicked");
+				int result;
+				String choosertitle = null;
+				
+			    	JFileChooser chooser = new JFileChooser(); 
+			    	chooser.setCurrentDirectory(new java.io.File("."));
+			    	chooser.setDialogTitle(choosertitle);
+			    	chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			   
+			    	chooser.setAcceptAllFileFilterUsed(false);
+			        
+			    	if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) { 
+			      		System.out.println("getCurrentDirectory(): "+  chooser.getCurrentDirectory());
+			      		System.out.println("getSelectedFile() : " +  chooser.getSelectedFile());}
+			    	else {
+			      		System.out.println("No Selection ");}
 				report.writeToFile();
 			}
 		});
