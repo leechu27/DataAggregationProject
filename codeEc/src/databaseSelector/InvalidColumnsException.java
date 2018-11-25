c7248f8e1277f3f6f84940c03fa937ecfb5f560e
Christopher Recillo <topher452@hotmail.com>
2018-11-25 13:20:46 -0500

    removed dead code

--- codeEc/src/databaseSelector/InvalidColumnsException.java
+++
@@ -1,9 +0,0 @@
-package databaseSelector;
-
-@SuppressWarnings("serial")
-public class InvalidColumnsException extends Exception {
-
-  public InvalidColumnsException() {
-    super("Columns not found");
-  }  
-}
--- codeEc/src/databaseSelector/InvalidTableException.java
+++
@@ -1,9 +0,0 @@
-package databaseSelector;
-
-@SuppressWarnings("serial")
-public class InvalidTableException extends Exception {
-
-  public InvalidTableException() {
-    super("Table not found");
-  }  
-}
