d893ca4d13c1c560ed9b95d0fc696dd28c307fab
Christopher Recillo <topher452@hotmail.com>
2018-11-26 14:39:24 -0500

    fixed mismatched feilds

--- codeEc/src/gui/TEQ.java
+++ codeEc/src/gui/TEQ.java
@@ -133,8 +133,8 @@ public class TEQ extends JFrame {
 			public void mouseClicked(MouseEvent arg0) {
 				System.out.println("Filter button clicked");
 				String tableInput = textField_1.getText();
-				String label = textField.getText();
-				String condition = textField_2.getText();
+				String condition = textField.getText();
+				String label = textField_2.getText();
 				System.out.println("Table: " + tableInput);
 				System.out.println("Condition: " + condition);
 				System.out.println("Label: " + label);
